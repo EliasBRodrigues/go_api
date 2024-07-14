a3779ffe8d2ad72bb5c801b4d44a3102c62b8e83
EliasBRodrigues <elias.rborges3@gmail.com>
2024-07-13 22:57:18 -0300

    fix: required project libs

--- main/main.go
+++ main/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"go-api/controller"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,13 +12,24 @@ import (
 func main() {
 	server := gin.Default()
 
-	ProductController := controller.NewProductController()
+	dbConnect, error := db.ConnectDB()
+	if error != nil {
+		panic(error)
+	}
+
+	ProductRepository := repository.NewProductRepository(dbConnect)
+	ProductService := service.NewProductService(ProductRepository)
+	ProductController := controller.NewProductController(ProductService)
 
 	server.GET("/api", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"message": "running"})
 	})
 
 	server.GET("/products", ProductController.GetProducts)
+	server.GET("/product/:productId", ProductController.GetProductsById)
+	server.POST("/products", ProductController.CreateProduct)
+	server.PUT("/products/:prodID", ProductController.UpdateProductById)
+	server.DELETE("/product/:prodID", ProductController.DeleteProductById)
 
-	server.Run(":5050")
+	server.Run(":8080")
 }
